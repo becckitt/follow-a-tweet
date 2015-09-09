ced62cafcf810e333c5f3b0c4a20067763e02e1c
Flatiron School <flatironschool@Flatirons-MacBook-Air-4.local>
2015-09-09 15:16:15 -0400

    adds twitter gem and adds skeleton for twitter class.

--- app/models/wrappers/twitter.rb
+++ app/models/wrappers/twitter.rb
@@ -1,1 +1,20 @@
-twitter.rb
+class fetchTweets
+	attr_reader :search_term
+
+	def initialize(search_term)
+		@search_term = search_term
+	end
+
+	TWITTER = Twitter::REST::Client.new do |config|
+		
+	end
+
+	def get_search
+		TWITTER.search(search_term).collect do |tweet|
+			tweet.user
+		end
+	end
+
+
+
+end
