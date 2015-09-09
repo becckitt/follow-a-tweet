e0a5cc36a6bd85e493d09942eb6c632bf8d609c6
Flatiron School <flatironschool@Flatirons-MacBook-Air-4.local>
2015-09-09 15:32:02 -0400

    adds figaro gem with api keys

--- app/models/wrappers/twitter.rb
+++ app/models/wrappers/twitter.rb
@@ -6,7 +6,10 @@ class fetchTweets
 	end
 
 	TWITTER = Twitter::REST::Client.new do |config|
-		
+		config.consumer_key = ENV["consumer_key"]
+  		config.consumer_secret = ENV["consumer_secret"]
+  		config.access_token = ENV["access_token"]
+  		config.access_token_secret = ENV["access_token_secret"]
 	end
 
 	def get_search
